2c1eb76b4f14259389267e1466e159bc93b5514f
KnightNikhil <nikhil.the.developer@gmail.com>
2025-04-12 13:15:15 +0530

    Initial commit

---
+++ src/test/java/com/nikhil/springboot/AtithiStay/AtithiStayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nikhil.springboot.AtithiStay;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AtithiStayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
